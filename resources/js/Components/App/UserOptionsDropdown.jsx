a1c22db8b0c14b367df57c30aec5e92531294216
Lindsey Verbauwhede <mora376@hotmail.com>
2024-07-29 17:03:41 +0200

    App folder gemaakt

---
+++ resources/js/Components/App/UserOptionsDropdown.jsx
@@ -0,0 +1,10 @@
+export default function UserOptionsDropdown() {
+    return (
+        <div>
+            <Menu as="div" className="relative inline-block text-left">
+
+            </Menu>
+        </div>
+    );
+}
+
--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -14,10 +14,21 @@ const ChatLayout = ({ children }) => {
     console.log("conversations", conversations);
     console.log("selectedConversation", selectedConversation);
 
+    const onSearch = (ev) => {
+        const search = ev.target.value.toLowerCase();
+        setLocalConversations(
+            conversations.filter((conversation) => {
+                return (
+                    conversation.name.toLowerCase().includes(search)
+                );
+            })
+        );
+    };
+
     useEffect(() => {
         setSortedConversations(
             localConversations.sort((a, b) => {
-                if(a.blocked_at && b.blocked_at) {
+                if (a.blocked_at && b.blocked_at) {
                     return a.blocked_at > b.blocked_at ? 1 : -1;
                 } else if (a.blocked_at) {
                     return 1;
@@ -76,28 +87,58 @@ const ChatLayout = ({ children }) => {
                 console.log("error", error);
             });
 
-            return () => {
-                Echo.leave("online");
-            };
+        return () => {
+            Echo.leave("online");
+        };
     }, []);
 
     return (
         <>
-        <div className="flex-1 w-full flex overflow-hidden">
-            <div
-                className={`transition-all w-full sm:w-[220px] md:w-[300px] bg-slate-800
-                    flex flex-col overflow-hidden ${selectedConversation ? "-ml-[100%] sm:ml-0" : ""}`}
-            >
-                <div className="">
-
+            <div className="flex-1 w-full flex overflow-hidden">
+                <div
+                    className={`transition-all w-full sm:w-[220px] md:w-[300px] bg-slate-800
+                    flex flex-col overflow-hidden ${
+                        selectedConversation ? "-ml-[100%] sm:ml-0" : ""
+                    }`}
+                >
+                    <div className="flex items-center justify-between py-2 px-3 text-xl font-medium">
+                        My Chats
+                        <div
+                            className="tooltip tooltip-left"
+                            data-tip="Create new Group"
+                        >
+                            <button className="text-gray-400 hover:text-gray-200">
+                                <PencilSquareIcon className="w-4 h-4 inline-block ml-2" />
+                            </button>
+                        </div>
+                    </div>
+                    <div className="p-3">
+                        <textInput
+                            onKeyUp={onSearch}
+                            placeholder="Filter users and groups"
+                            className="w-full"
+                        />
+                    </div>
+                    <div className="flex-1 overflow-auto">
+                        {sortedConversations &&
+                        sortedConversations.map((conversation) => (
+                            <ConversationItem
+                                key={`${
+                                    conversation.is_group
+                                    ? "group_"
+                                    : "user_"
+                                }${conversation.id}`}
+                                conversation={conversation}
+                                online={!!isUserOnline(conversation.id)}
+                                selectedConversation={selectedConversation}
+                            />
+                        ))}
+                    </div>
+                </div>
+                <div className="flex-1 flex flex-col overflow-hidden">
+                    {children}
                 </div>
             </div>
-            <div className="flex-1 flex flex-col overflow-hidden">
-                {children}
-            </div>
-
-        </div>
-
         </>
     );
 };
