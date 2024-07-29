a03a1070e8b41caa6cf34b2fcec825e825f675db
Lindsey Verbauwhede <mora376@hotmail.com>
2024-07-29 15:07:36 +0200

    Chat logo toegevoegd en toegepast

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -5,9 +5,9 @@ export default function Guest({ children }) {
     return (
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
-                <Link href="/">
-                    <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
-                </Link>
+            <Link href="/" className="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200">
+                <img src="/img/chat.png" alt="Chat Logo" className="block h-12 w-auto mb-4" />
+            </Link>
             </div>
 
             <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
