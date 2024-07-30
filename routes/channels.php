919985a738f914373915bb71f83555c18502d6f7
Lindsey Verbauwhede <mora376@hotmail.com>
2024-07-30 17:43:03 +0200

    Display members and messages

--- routes/channels.php
+++ routes/channels.php
@@ -1,8 +1,17 @@
 <?php
 
 use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('online', function ($user) {
+Broadcast::channel('online', function (User $user) {
     return $user ? new UserResource($user) : null;
+});
+
+Broadcast::channel('message.user.{userId1}-{userId2}', function (User $user, int $UserId1, int $UserId2) {
+    return $user->id === $UserId1 || $user->id === $UserId2 ? $user : null;
+});
+
+Broadcast::channel('message.group.{groupId}', function (User $user, int $groupId) {
+    return $user->groups->contains('id', $groupId) ? $user : null;
 });
